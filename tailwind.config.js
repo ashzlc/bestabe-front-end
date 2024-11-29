a196fcf80f48b70d72b91de54251b8d67d77aa50
ashzlc <aakash.internest@gmail.com>
2024-11-29 14:45:56 +0530

    src file update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}" , "./index.html"],
+  content: ["./src/**/*.{html,js}, ./html"],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'red': 'ff0000',
-      'primaryblue': '#f8e63a',
-      'tahiti': {
-        100: '#cffafe',
-        200: '#a5f3fc',
-        300: '#67e8f9',
-        400: '#22d3ee',
-        500: '#06b6d4',
-        600: '#0891b2',
-        700: '#0e7490',
-        800: '#155e75',
-        900: '#164e63',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
+
